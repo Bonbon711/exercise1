198c0eabbaa0e8a0b4726ccca6e58899f42f8909
Bonbon711 <22102183@usc.edu.ph>
2024-09-03 20:56:50 +0800

    Add greeting feature

--- exercise_1.js
+++ exercise_1.js
@@ -1,2 +1,13 @@
-console.log("Hello World");
+
+console.log("Hello, World");
 console.log("Hello Earth");
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+    });
+
+readline.question('Please enter your name: ', name => {
+    console.log(`Hello, ${name}!`);
+    readline.close();
+  });
